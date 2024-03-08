cec30a808977086f1ee45e485f06f2ed2ef6078d
Harry Chen <harry.chen@uwaterloo.ca>
2024-03-07 22:19:41 -0800

    added 3 different sizes

--- app.py
+++ app.py
@@ -20,6 +20,25 @@ def submit_domain():
     if not validators.domain(domain):
         return "Invalid domain. Please enter a valid domain."
 
-    favicon_url = f"http://www.google.com/s2/favicons?sz=64&domain_url={domain}"
+    favicon_url_16 = f"http://www.google.com/s2/favicons?sz=16&domain_url={domain}"
+    favicon_url_64 = f"http://www.google.com/s2/favicons?sz=64&domain_url={domain}"
+    favicon_url_256 = f"http://www.google.com/s2/favicons?sz=256&domain_url={domain}"
 
-    return f'<img src="{favicon_url}" alt="Favicon"><br><a href="{favicon_url}">Click here to get favicon</a>'
+    return f'''
+    <h1>Showing favicons for: {domain}</h1>
+    <div>
+        <img src="{favicon_url_16}" alt="Favicon 16x16">
+        <p>16x16</p>
+        <a href="{favicon_url_16}" download>Download 16x16</a>
+    </div>
+    <div>
+        <img src="{favicon_url_64}" alt="Favicon 64x64">
+        <p>64x64</p>
+        <a href="{favicon_url_64}" download>Download 64x64</a>
+    </div>
+    <div>
+        <img src="{favicon_url_256}" alt="Favicon 256x256">
+        <p>256x256</p>
+        <a href="{favicon_url_256}" download>Download 256x256</a>
+    </div>
+    '''
